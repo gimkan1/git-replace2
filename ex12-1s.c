40bf4d3444861366fda9923e07cb1535331a789c
gimkan1 <gimkan1@gmail.com>
2022-01-10 10:13:19 +0900

    devide history

---
+++ ex12-1s.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <unistd.h>
+
+#define SIZE    sizeof(struct sockaddr_in)
+
+// 작업1 6584676
+// 작업2
+
+int main()
+{
+    int sockfd_listen;
+    int sockfd_connect;
+
+    char c;
+
+    struct sockaddr_in server = {AF_INET, 5000, INADDR_ANY};
+
+    printf("socket()\n");
+    sockfd_listen = socket(AF_INET, SOCK_STREAM, 0);
+
+    printf("bind()\n");
+    bind(sockfd_listen, (struct sockaddr *)&server, SIZE);
+
+    printf("listen()\n");
+    listen(sockfd_listen, 5);
+
+    printf("wating for client\n");
+    sockfd_connect = accept(sockfd_listen, NULL, NULL);
+    printf("accepted\n");
+
+    recv(sockfd_connect, &c, 1, 0);
+    printf("recv %c from client\n", c);
+
+    c++;
+    printf("send %d to client\n", c);
+    send(sockfd_connect, &c , 1, 0);
+
+    printf("close()\n");
+    close(sockfd_connect);
+    close(sockfd_listen);
+
+    return 0;
+}
